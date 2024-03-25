1b33977744f948e1ff33cdf2c68acb3ae7504f3b
ardila <146051568+putuardila@users.noreply.github.com>
2024-03-25 12:17:10 +0800

    senin/25/03/2024

---
+++ Phython/meeet1.py
@@ -0,0 +1,7 @@
+#perkenalan diri MK.Struktur data
+
+print("Nama saya Ni Putu ardila")
+print("NIM 23241054")
+print("Semester II/B")
+print("Saya mahasiswa Undikma")
+print("Saya mengambil prodi PTI")
