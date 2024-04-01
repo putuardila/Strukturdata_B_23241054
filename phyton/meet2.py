f9ce52d74f45e1d4ff738f84f55df0bc0c33a927
ardila <146051568+putuardila@users.noreply.github.com>
2024-04-01 12:01:34 +0800

    senin/01/04/2024

---
+++ phyton/meet2.py
@@ -0,0 +1,19 @@
+# Belajar Variabel
+# Assigment
+x = 3
+y = 6
+uang = 1000
+
+#pemanggilan
+print ("Nilai UAS saya =", x)
+print ("Nilai UTS saya =", y)
+print ("sisa uang saya =", uang)
+
+# penamaan variabel
+nilai_a = 9
+kotak10 = 10
+
+# input variabel kedua
+x = 30
+print ("Nilai UAS saya =", x)
+
