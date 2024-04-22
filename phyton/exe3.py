d5854c658dd3667a063c063c08cd3067aba8acbb
ardila <146051568+putuardila@users.noreply.github.com>
2024-04-22 11:47:50 +0800

    senin 22/04/2024

---
+++ phyton/exe3.py
@@ -0,0 +1,20 @@
+# contoh tipe data (integer) : angka satuan
+bil_integer = 1
+print(type(bil_integer))
+
+print ("data dari bil_integer adalah: ", type (bil_integer ))
+#contoh tipe data (float) : bilangan dessimal
+bil_float = 2.5
+print ("data dari bil_float adalah: ", type (bil_float ))
+ #contoh tipe data (string) : kumpulan karakter
+bil_string = 'putu'
+print ("data dari bil_string adalah: ", type (bil_string ))
+
+#contoh tipe data (bolean) : biner, 0,1, True False
+bil_bol= True
+print ("data dari bil_bol adalah: ", type (bil_bol ))
+
+#tipe data dari bahasa c 
+from ctypes import c_double
+bil_double = c_double(2000000)
+print ("type data bil_double adalah: ",type(bil_double))
