3dcb3c3522260a216e5a11291bfc7756f803d25e
ardila <146051568+putuardila@users.noreply.github.com>
2024-05-24 19:07:59 +0800

    senin 20,mei 2024

---
+++ meet8.py
@@ -0,0 +1,20 @@
+# perbandingan Lanjutan
+
+# +++++3 --------9+++++++++
+MasukanUser = float(input("masukan bilangan \nkurang dari 3 atau \nlebih dari 9 : "))
+
+# ----cek kurang dari 3
+kurDar = (MasukanUser < 3)
+
+# ----cek kurang dari 9
+lebDar = (MasukanUser < 9)
+
+print(kurDar or lebDar)
+
+
+
+
+
+
+# 4 14
+MasukanUser = float(input("masukan bilangan 'antara' 4  dan 14"))
