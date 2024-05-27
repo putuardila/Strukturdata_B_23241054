d1840e040be5b5d3ee87781fd4a2bfa54c5ba642
ardila <146051568+putuardila@users.noreply.github.com>
2024-05-27 10:09:48 +0700

    27 Mei 2024

---
+++ meet4&5.py
@@ -0,0 +1,49 @@
+# infut dari user 
+# aritmatika 
+
+# belajar infutan
+sandi= input("masukan kata : ")
+print("isi dari sandi: ", sandi,"bertipe data :",type (sandi))
+
+# belajar aritmatika dasar 
+x = 4
+y = 9
+
+# perjumlahan + 
+hasil = x + y 
+print("x + y =", hasil) 
+
+# pengurangan -
+hasil = x - y
+print("x - y =", hasil)
+
+# perkalian *
+hasil = x * y
+print("x * y =", hasil)
+
+# pembagian /
+hasil = x / y
+print("x / y =", hasil)
+
+# perpangkatan exponen **
+hasil = x ** y 
+print("x ** y =", hasil)
+
+# modulus%
+hasil = y % x
+print("x mood y =", hasil)
+
+#floor division (pembulatan kebawah)
+hasil = x // y 
+print("x // y =", hasil)
+
+
+#aritmatika prioritas
+# (), exponen, perkalian / pembagian, pejumlahan/ pengurangan 
+x = 3 
+y = 4
+z = 5
+
+hasil = (x ** y *(z + x ) / y - z % x // y )
+print(hasil)
+
