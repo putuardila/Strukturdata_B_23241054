7122ebfe35b87fa778b9ecf1f4d0497d13c23331
ardila <146051568+putuardila@users.noreply.github.com>
2024-06-03 10:46:33 +0700

    03-06-2024

---
+++ meet9.py
@@ -0,0 +1,16 @@
+# Percabangan jika maka
+
+name = input("what is your name")
+print (name)
+
+#if kondisi : aksi
+
+#if standar
+#if name == "edy" : print(Dosen ganteng)
+#print("end of program")
+
+#if indentasi
+if name=="edy":
+    print("dosen ganteng")
+    print("ganteng banget")
+print("end of program")
